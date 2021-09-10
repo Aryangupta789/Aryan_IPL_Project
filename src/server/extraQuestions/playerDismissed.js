3cc9c82e75808ff538060af43cbbfd26589ad9b5
Aryangupta789 <iamaryangupta78@gmail.com>
2021-09-10 11:59:26 +0530

    updated player Dismissed Question

--- src/server/extraQuestions/playerDismissed.js
+++ src/server/extraQuestions/playerDismissed.js
@@ -1,6 +1,6 @@
 //Find the highest number of times one player has been dismissed by another player
 function playerDismissed(deliveries) {
-  let finalResult = {};
+  let result = {};
 
   let playerDismissedData = deliveries.filter(
     (item) => item.player_dismissed !== ""
@@ -12,9 +12,21 @@ function playerDismissed(deliveries) {
         bowlerName[delivery.bowler] = (bowlerName[delivery.bowler] || 0) + 1;
       }
     });
-    finalResult[player.batsman] = bowlerName;
+    result[player.batsman] = bowlerName;
   });
- 
+  console.log(result)
+  finalResult={}
+  for(player in result){
+      let max=Math.max(...Object.values(result[player]))
+      let obj =result[player];
+      
+      for (iterate in obj){
+          if (obj[iterate]===max){
+              finalResult[player]= [iterate +":"+ max]
+
+          }
+      }
+    }
   return finalResult;
 }
 module.exports = playerDismissed;
